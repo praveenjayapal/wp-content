54f74e0c2dfdba75b2951d83cf7a1322c34c48b6
barathwaj555 <barathwaj555@gmail.com>
2016-04-28 14:45:19 +1000

    Done soem changes to the styles and appearances of the comment session in every post

--- themes/barrier-reef-orchestra/comments.php
+++ themes/barrier-reef-orchestra/comments.php
@@ -28,9 +28,9 @@ if ( post_password_required() ) {
 		<h2 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'barrier-reef-orchestra' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
+					esc_html( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'barrier-reef-orchestra' ) ),
+					number_format_i18n( get_comments_number() )
+
 				);
 			?>
 		</h2>
@@ -52,6 +52,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+					'avatar_size' => '64'
 				) );
 			?>
 		</ol><!-- .comment-list -->
