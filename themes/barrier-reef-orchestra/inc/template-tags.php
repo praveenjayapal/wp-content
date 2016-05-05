9a8b6c2a2480c03d7702c56359ffa977887857fd
barathwaj555 <barathwaj555@gmail.com>
2016-05-05 15:48:50 +1000

    Added some styles to archive pages and index pages

--- themes/barrier-reef-orchestra/inc/template-tags.php
+++ themes/barrier-reef-orchestra/inc/template-tags.php
@@ -58,6 +58,47 @@ function barrier_reef_orchestra_posted_on() {
 }
 endif;
 
+
+if ( ! function_exists( 'barrier_reef_orchestra_index_posted_on' ) ) :
+	/**
+	 * Prints HTML with meta information for post-date/time and author on index pages.
+	 */
+	function barrier_reef_orchestra_index_posted_on() {
+
+		$author_id = get_the_author_meta( 'ID' );
+
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_html( get_the_modified_date() )
+		);
+		$posted_on = sprintf(
+			esc_html_x( 'Published %s', 'post date', 'barrier_reef_orchestra' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+		$byline = sprintf(
+			esc_html_x( 'by %s', 'post author', 'barrier_reef_orchestra' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+
+		echo '<div class="meta-content">';
+		echo '<span class="byline">' . $byline . ' </span><span class="posted-on">' . $posted_on . ' </span>'; // WPCS: XSS OK.
+		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link( esc_html__( 'Leave a comment', 'barrier_reef_orchestra' ), esc_html__( '1 Comment', 'barrier_reef_orchestra' ), esc_html__( '% Comments', 'barrier_reef_orchestra' ) );
+			echo '</span>';
+		}
+		echo '</div><!-- .meta-content -->';
+	}
+endif;
+
+
+
 if ( ! function_exists( 'barrier_reef_orchestra_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
--- themes/barrier-reef-orchestra/template-parts/content.php
+++ themes/barrier-reef-orchestra/template-parts/content.php
@@ -8,40 +8,35 @@
  */
 
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+		<?php the_title( sprintf( '<h2 class="entry-title index-excerpt"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php barrier_reef_orchestra_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="index-entry-meta">
+				<?php barrier_reef_orchestra_index_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content index-excerpt">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'barrier-reef-orchestra' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'barrier-reef-orchestra' ),
-				'after'  => '</div>',
-			) );
+		the_excerpt();
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php barrier_reef_orchestra_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<div class="continue-reading">
+		<a href="<?php echo esc_url(get_permalink()); ?>" rel="bookmark" >
+			<?php
+			printf(
+				wp_kses( __( 'Continue reading %s ', 'barrier_reef_orchestra' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) ;
+
+
+			?>
+		</a>
+	</div>
+
+	
 </article><!-- #post-## -->
